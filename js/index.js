6faa26dbdcd63a2154c57602c6f02a3d26782cc6
DimejiAre <olardi2@gmail.com>
2019-09-03 12:30:25 +0100

    create event listener to change anchor color to orange on mouse over

--- js/index.js
+++ js/index.js
@@ -6,3 +6,16 @@ document.querySelectorAll('a').forEach((anchor)=>{
         event.preventDefault();
     })
 })
+
+const navAnchors = document.querySelectorAll('a')
+
+// Change anchor color to orange on mouse over
+navAnchors.forEach((anchor) => {
+    anchor.addEventListener('mouseover', (event) => {
+        event.target.style.color = "orange";
+
+        setTimeout(function() {
+            event.target.style.color = "";
+          }, 1000);
+    })
+})
