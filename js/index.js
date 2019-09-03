8e0bde81e522ec73fa299b62deabd38a1071ee52
DimejiAre <olardi2@gmail.com>
2019-09-03 12:21:02 +0100

    stop navigation from refreshing page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// Stop Navigation from Refreshing Page
+document.querySelectorAll('a').forEach((anchor)=>{
+    anchor.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
+})
